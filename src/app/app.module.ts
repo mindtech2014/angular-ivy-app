8fc9c4e1529e85aac7d6708d7e9a03e077707422
mindtech2014 <techmind2014@gmail.com>
2020-07-31 16:14:34 +0530

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NGMaterialModule } from './material-module/material-module';
+import { HeaderComponent } from './header/header.component';
+import { SampleComponent } from './sample/sample.component';
+
+
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, NGMaterialModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, SampleComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
