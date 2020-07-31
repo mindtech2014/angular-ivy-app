9a23e51f20b41f8cea42552ab6e7535927acbde3
mindtech2014 <techmind2014@gmail.com>
2020-07-31 19:44:37 +0530

    6 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NGMaterialModule } from './material-module/material-module';
 import { HeaderComponent } from './header/header.component';
-import { SampleComponent } from './sample/sample.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, NGMaterialModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, SampleComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent,  SidebarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
